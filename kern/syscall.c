69c07e6bfd214da893da9367686a902ba934be9f
Georgia Griffin <thegeorgiagrace@gmail.com>
2026-04-06 22:24:50 -0400

    Lab 4 complete

--- kern/syscall.c
+++ kern/syscall.c
@@ -349,7 +349,47 @@ static int
 sys_ipc_try_send(envid_t envid, uint32_t value, void *srcva, unsigned perm)
 {
 	// LAB 4: Your code here.
-	panic("sys_ipc_try_send not implemented");
+	struct Env *dst;
+    if (envid2env(envid, &dst, 0) < 0)
+        return -E_BAD_ENV;
+
+    if (!dst->env_ipc_recving)
+        return -E_IPC_NOT_RECV;
+
+    if ((uintptr_t)srcva < UTOP) {
+        if ((uintptr_t)srcva % PGSIZE != 0)
+            return -E_INVAL;
+
+        // Use PTE_SYSCALL as the allowed permission mask, same as sys_page_alloc
+        if ((perm & PTE_SYSCALL) != perm)
+            return -E_INVAL;
+        if ((perm & (PTE_U | PTE_P)) != (PTE_U | PTE_P))
+            return -E_INVAL;
+
+        pte_t *pte;
+        struct PageInfo *pp = page_lookup(curenv->env_pgdir, srcva, &pte);
+        if (!pp)
+            return -E_INVAL;
+
+        if ((perm & PTE_W) && !(*pte & PTE_W))
+            return -E_INVAL;
+
+        if ((uintptr_t)dst->env_ipc_dstva < UTOP) {
+            if (page_insert(dst->env_pgdir, pp, dst->env_ipc_dstva, perm) < 0)
+                return -E_NO_MEM;
+            dst->env_ipc_perm = perm;
+        } else {
+            dst->env_ipc_perm = 0;
+        }
+    } else {
+        dst->env_ipc_perm = 0;
+    }
+
+    dst->env_ipc_recving = 0;
+    dst->env_ipc_from = curenv->env_id;
+    dst->env_ipc_value = value;
+    dst->env_status = ENV_RUNNABLE;
+    return 0;
 }
 
 // Block until a value is ready.  Record that you want to receive
@@ -367,8 +407,15 @@ static int
 sys_ipc_recv(void *dstva)
 {
 	// LAB 4: Your code here.
-	panic("sys_ipc_recv not implemented");
-	return 0;
+	if ((uintptr_t)dstva < UTOP && (uintptr_t)dstva % PGSIZE != 0)
+        return -E_INVAL;
+
+    curenv->env_ipc_recving = 1;
+    curenv->env_ipc_dstva = dstva;
+    curenv->env_status = ENV_NOT_RUNNABLE;
+    curenv->env_tf.tf_regs.reg_eax = 0;  // return 0 when resumed
+    sched_yield();  // never returns
+    return 0;
 }
 
 // Dispatches to the correct kernel function, passing the arguments.
@@ -415,6 +462,12 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 
 	case SYS_env_set_pgfault_upcall:
 		return sys_env_set_pgfault_upcall((envid_t)a1, (void *)a2);
+
+	case SYS_ipc_recv:
+    	return sys_ipc_recv((void *)a1);
+
+	case SYS_ipc_try_send:
+		return sys_ipc_try_send((envid_t)a1, (uint32_t)a2, (void *)a3, (unsigned)a4);
 
 	default:
 		return -E_INVAL;
--- kern/trap.c
+++ kern/trap.c
@@ -88,6 +88,24 @@ void t_mchk();
 void t_simderr();
 void t_syscall();
 
+// lab 4 exercise 13
+void irq_0();
+void irq_1();
+void irq_2();
+void irq_3();
+void irq_4();
+void irq_5();
+void irq_6();
+void irq_7();
+void irq_8();
+void irq_9();
+void irq_10();
+void irq_11();
+void irq_12();
+void irq_13();
+void irq_14();
+void irq_15();
+
 
 void
 trap_init(void)
@@ -125,6 +143,24 @@ trap_init(void)
     SETGATE(idt[T_MCHK],    0, GD_KT, t_mchk,    0);
     SETGATE(idt[T_SIMDERR], 0, GD_KT, t_simderr, 0);
 	SETGATE(idt[T_SYSCALL], 0, GD_KT, t_syscall, 3);
+
+	// lab 4 exercise 13
+	SETGATE(idt[IRQ_OFFSET + 0],  0, GD_KT, irq_0,  0);
+	SETGATE(idt[IRQ_OFFSET + 1],  0, GD_KT, irq_1,  0);
+	SETGATE(idt[IRQ_OFFSET + 2],  0, GD_KT, irq_2,  0);
+	SETGATE(idt[IRQ_OFFSET + 3],  0, GD_KT, irq_3,  0);
+	SETGATE(idt[IRQ_OFFSET + 4],  0, GD_KT, irq_4,  0);
+	SETGATE(idt[IRQ_OFFSET + 5],  0, GD_KT, irq_5,  0);
+	SETGATE(idt[IRQ_OFFSET + 6],  0, GD_KT, irq_6,  0);
+	SETGATE(idt[IRQ_OFFSET + 7],  0, GD_KT, irq_7,  0);
+	SETGATE(idt[IRQ_OFFSET + 8],  0, GD_KT, irq_8,  0);
+	SETGATE(idt[IRQ_OFFSET + 9],  0, GD_KT, irq_9,  0);
+	SETGATE(idt[IRQ_OFFSET + 10], 0, GD_KT, irq_10, 0);
+	SETGATE(idt[IRQ_OFFSET + 11], 0, GD_KT, irq_11, 0);
+	SETGATE(idt[IRQ_OFFSET + 12], 0, GD_KT, irq_12, 0);
+	SETGATE(idt[IRQ_OFFSET + 13], 0, GD_KT, irq_13, 0);
+	SETGATE(idt[IRQ_OFFSET + 14], 0, GD_KT, irq_14, 0);
+	SETGATE(idt[IRQ_OFFSET + 15], 0, GD_KT, irq_15, 0);
 
 	// Per-CPU setup
 	trap_init_percpu();
@@ -260,6 +296,11 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle clock interrupts. Don't forget to acknowledge the
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
+		lapic_eoi();
+		sched_yield();
+		return;
+	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
